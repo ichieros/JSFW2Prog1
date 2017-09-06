1d0ecc61bdb9730f02e38badc7fff9edb8eceea2
jsf3 <jsf3@jsf3-virtual-machine>
2017-09-06 11:09:31 +0200

    Created Project

---
+++ JSFW2P1JavaShell/src/jsfw2p1javashell/JSFW2P1JavaShell.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsfw2p1javashell;
+
+/**
+ *
+ * @author jsf3
+ */
+public class JSFW2P1JavaShell {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
