036af33e81772720d2472cd3b78e0d8945097bc0
jsf3 <jsf3@jsf3-virtual-machine>
2017-09-06 12:33:27 +0200

    Finished P1 created P2

---
+++ JSFW2P2/src/jsfw2p2/JSFW2P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsfw2p2;
+
+/**
+ *
+ * @author jsf3
+ */
+public class JSFW2P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
